438b75376bf26e94037136830a65f5eb437b2be1
dfagan <dfagan@emoneyadvisor.com>
2021-01-22 16:16:47 -0500

    Add click event to drop markers

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,16 +2,24 @@
 	import { onMount } from 'svelte';
 	import mapboxgl from 'mapbox-gl';
 
+	let map;
+
 	onMount(() => {
 		mapboxgl.accessToken = 'pk.eyJ1IjoibGFtb2IiLCJhIjoiY2trOG10YWd0MG54ZTJ6bzBienFlNDBpdCJ9.e65J6b9meUMMK7s2UEvWWQ';
-		const map = new mapboxgl.Map({
+		map = new mapboxgl.Map({
 			container: 'map',
 			style: 'mapbox://styles/mapbox/streets-v11',
 			center: [-71.411788, 41.825188],
 			zoom: 15
 		});
 
-		var marker = new mapboxgl.Marker()
+		map.on('click', event => {
+			new mapboxgl.Marker()
+				.setLngLat(event.lngLat)
+				.addTo(map);
+		});
+
+		new mapboxgl.Marker()
 			.setLngLat([-71.411788, 41.825188])
 			.addTo(map);
 	});
