476346fb11ace6469b198b9441285ca358440909
dfagan <dfagan@emoneyadvisor.com>
2021-01-22 14:10:00 -0500

    Remove Nav from main layout

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,8 +1,6 @@
-<script>
-	import Nav from '../components/Nav.svelte';
-
-	export let segment;
-</script>
+<main>
+	<slot></slot>
+</main>
 
 <style>
 	main {
@@ -14,9 +12,3 @@
 		box-sizing: border-box;
 	}
 </style>
-
-<Nav {segment}/>
-
-<main>
-	<slot></slot>
-</main>
